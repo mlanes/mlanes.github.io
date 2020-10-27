071c6c6a28d92c06a95413fbad35997d8aa8e2bb
Mário Lanes <mario.lanesjr@gmail.com>
2020-10-27 04:20:08 +0000

    Adding script to do a smooth scrolling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,5 +2,29 @@ $(document).ready(function () {
   // Menu Mobile Initialization
   $('.sidenav').sidenav();
   // Paralax Initialization
-  $('.parallax').parallax();
+  // $('.parallax').parallax();
+
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function (event) {
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, () => {
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".js-scroll-trigger").click(() => {
+    // $('.sidenav').sidenav('close');
+  });
 });
